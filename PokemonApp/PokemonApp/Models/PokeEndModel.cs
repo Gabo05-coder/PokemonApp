79168f51e0ba8fbd04661133b576a631516a39f0
Gabo <gabriel.quintana@utp.ac.pa>
2025-02-11 18:26:58 -0500

    commit?

---
+++ PokemonApp/PokemonApp/Models/PokeEndModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonApp.Models;
+public class Pokemon
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
+
+public class PokemonResponse
+{
+    public List<Pokemon> Results { get; set; }
+}
